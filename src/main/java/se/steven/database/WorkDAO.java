c2724c51da6365819286374e730c04bf0c4d48e7
StevenJTK <114819398+StevenJTK@users.noreply.github.com>
2024-12-19 16:58:01 +0100

    Final build.

--- src/main/java/se/steven/database/WorkDAO.java
+++ src/main/java/se/steven/database/WorkDAO.java
@@ -5,23 +5,14 @@ import java.util.List;
 
 public interface WorkDAO {
 
-    //anropa metoden med en nu instans av Role // COMPLETED
     public void insertRole(Role role) throws SQLException;
 
-
-    //se metoden för ändraingar//  COMPLETED
     public List<String> fetchAllRoles() throws SQLException;
 
-
-    //anropa metoden från main - den här metoden är färdig// COMPLETED
     public Role fetchOneRole(int roleId) throws SQLException;
 
-
-    //skcika med paramter för roleId, samma som fetchOneROle// COMPLETED
     public void updateRole(int roleId) throws SQLException;
 
-
-    //skicka med paramter för roleid, samma som ovan// COMPLETED
     public void removeRole(int roleId) throws SQLException;
 
 }
